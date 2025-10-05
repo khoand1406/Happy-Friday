8e9237241df222cda1378274f09e4b425ddcdeea
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2025-10-05 23:29:11 +0700

    Update ApiConstraint.ts

--- frontend/src/constraint/ApiConstraint.ts
+++ frontend/src/constraint/ApiConstraint.ts
@@ -1,5 +1,5 @@
 // export const BaseURl= "http://localhost:3000"
-export const BaseURl= "https://happyfriday-e9fbdygvebf9e6dv.southeastasia-01.azurewebsites.net/"
+export const BaseURl= "https://happyfriday-e9fbdygvebf9e6dv.southeastasia-01.azurewebsites.net"
 export const LOGIN= '/api/auth/login'
 
 export const USER_PROFILE= '/api/users/me'
